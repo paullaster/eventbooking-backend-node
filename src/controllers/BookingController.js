0a4c5701af54de56ae1dc4a99b59a44e16ddf257
paullaster <paullasterokoth98@gmail.com>
2024-03-09 23:04:29 +0300

    init node event booking backend

---
+++ src/controllers/BookingController.js
@@ -0,0 +1,14 @@
+class BookingController {
+    async createBooking(req, res) {
+        res.send('Hello World!');
+    }
+    async getBookings(req, res) {
+        res.send('Hello World!');
+    }
+    async updateBooking(req, res) {
+        res.send('Hello World!');
+    }
+    async deleteBooking(req, res) {
+        res.send('Hello World!');
+    }
+} 
