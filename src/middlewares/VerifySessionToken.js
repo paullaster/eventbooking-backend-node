9fc0a8fb7252fa6c76a148c606f8b83a7cfb19ef
paullaster <paullasterokoth98@gmail.com>
2024-04-08 21:43:30 +0300

    update(core): verify user token

--- src/middlewares/VerifySessionToken.js
+++ src/middlewares/VerifySessionToken.js
@@ -5,6 +5,7 @@ import { User } from "../db/UserModel.js";
 class VerifySessionTokenController {
     async verify(req, res, next) {
         try {
+            if(!req.headers.authorization) return res.ApiResponse.error(null, "Unauthorized", 403);
             const token = req.headers.authorization.split(" ")[1];
             const decoded = jwt.verify(token, app.key);
             const user = await User.findOne({
